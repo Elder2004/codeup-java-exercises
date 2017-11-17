4680d588da6238fe14601137f50948c26ae8ff52
Tony Westbrook <tonywestbrook58@mail.com>
2017-11-16 20:23:10 -0600

    first Java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,57 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+//        System.out.println("Hello, Welcome to Java World!");
+//
+//        int myFavoriteNumber = 60;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Java class is just beginning!";
+//        System.out.println(myString);
+//
+//        myString = 'a';
+//        myString =3.141;
+//        float myNumber;
+//        myNumber = 3.14L;
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//       String class = "my House";
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x =4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//       System.out.println(x);
+//       System.out.print(y);
     }
 }
