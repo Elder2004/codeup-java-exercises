ebfba00f6c17e65bd7dc19b7b28e17c4a23483cd
Tony Westbrook <tonywestbrook58@mail.com>
2017-11-17 16:49:33 -0600

    Java exercise console and control flow exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+//      1.
+
+//        int userInput;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter something: ");
+//        userInput = sc.nextInt();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+//        2.
+
+        //Declare Variables
+
+//        String name1;
+//        String name2;
+//        String name3;
+//
+//        //Accept User Imput
+
+//        Scanner keyboard=new Scanner(System.in);
+//        System.out.print("Please Enter First Name ");
+//        name1=keyboard.nextLine();
+//        System.out.print("Please Enter Second Name ");
+//        name2=keyboard.nextLine();
+//        System.out.print("Please Enter Third Name ");
+//        name3=keyboard.nextLine();
+
+//        System.out.println(name1);
+//        System.out.println(name2);
+//        System.out.println(name3);
+
+//        3.
+
+//           String mySentence;
+//           Scanner keyboard=new Scanner(System.in);
+//           System.out.println("Please write a sentence....");
+//           mySentence=keyboard.nextLine();
+//           System.out.print(mySentence);
+
+//         4.
+
+//
+    }
+}
+
+// 1. Exception in thread "main" java.util.InputMismatchException
+//at java.base/java.util.Scanner.throwFor(Scanner.java:860)
+
+//   3.do you capture all of the words? not with .next, but with  .nextline
---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+
+//       1a. loops Basic
+
+//
+//        int count = 5;
+//        while (count <= 15) {
+//            System.out.println("Count is: " + count);
+//            count++;
+
+
+//          1b. Do while
+
+//            int counter = 0;
+//            do {
+//                counter +=2;
+//                System.out.println("Count is: " + counter);
+//
+//            } while (counter < 100);
+
+//
+//
+//            int counter = 100;
+//            do {
+//                System.out.println("Count is: " + counter);
+//                --counter;
+//            } while (counter >= -10);
+
+
+//                long counter = 2;
+//                do {
+//                    counter *= counter;
+//                    System.out.println("Count is: " + counter);
+//
+//                } while (counter <= 10000);
+
+
+        }
+
+
+}
+
+
+
