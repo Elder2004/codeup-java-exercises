1b75071ce69312f5b17d7034100c47126af21717
Tony Westbrook <tonywestbrook58@mail.com>
2017-11-19 19:16:09 -0600

    Java exercise console and control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.util.Formatter;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
@@ -39,12 +41,49 @@ public class ControlFlowExercises {
 //                } while (counter <= 10000);
 
 
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(String.valueOf(i));
+//            }
+
+//
+//
 
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a Number to start the Power Table....");
+        int start = scan.nextInt();
+
+
+        System.out.println(" Here  is your Power Table!\n");
+        System.out.println("number  | squared | cubed");
+        System.out.println("------  | ------- | -----");
+
+            Formatter fmt;
+
+            for (int i = 1; i <= start; i++) {
+                fmt = new Formatter();
+
+                fmt.format("%4d    |%4d     | %4d", i, i * i, i * i * i);
+                System.out.println(fmt);
+            }
+
+
+//        int  getGrade = scan.nextInt();
+//        for (int i = 100; i >= 0; i--) {
+//            System.out.println(i + " =  :" + getGrade (i));
+        }
         }
 
 
-}
+
+
 
 
 
